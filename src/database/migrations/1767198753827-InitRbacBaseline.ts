40d9dfcaa0a3f32a3b733ec1248f42dd3b668c4f
anhnamnguyen0203 <anhnamnguyen0203@gmail.com>
2025-12-31 23:33:20 +0700

    feat(identity): baseline RBAC

---
+++ src/database/migrations/1767198753827-InitRbacBaseline.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitRbacBaseline1767198753827 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
