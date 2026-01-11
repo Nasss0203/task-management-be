87c0f25cf45b03f5df17b2a12b41bf81dc7f2527
anhnamnguyen0203 <anhnamnguyen0203@gmail.com>
2026-01-11 15:52:27 +0700

    feat(http exception)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { MyLogger } from './log/my.logger';
 
@@ -11,12 +12,15 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
-
+  const httpAdapterHost = app.get(HttpAdapterHost);
   // app.useGlobalGuards(new JwtAuthGuard(reflector));
   // app.useGlobalGuards(new PermissionGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
-
+  app.useGlobalFilters(
+    // new HttpExceptionFilter(),
+    new HttpExceptionFilter(httpAdapterHost),
+  );
   app.useLogger(new MyLogger());
   app.use(cookieParser());
   app.enableCors({
